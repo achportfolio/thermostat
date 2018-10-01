8b8f2b3780fbfabc04125559286e4d8a115c2ec3
Aidan Christopher Haughey <nazghoul22@gmail.com>
2018-09-30 21:16:55 -0700

    100% of functionality complete; 80% of design complete

--- src/components/Thermostat.js
+++ src/components/Thermostat.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import './Thermostat.css';
+import Roundy from 'roundy';
 
 class Thermostat extends Component {
   constructor() {
     super();
-    this.state={readout: 75,}
+    this.state={readout: 75, value: 75}
 
     this.handleChange = this.handleChange.bind(this);
   };
+
+  componentDidMount() {
+  }
 
   handleChange(event) {
     this.setState({readout: event.target.value});
   }
 
   render() {
+
+  	//<input onChange={this.handleChange} id="slider" name="slider" type="range" min="1" max="100" className="TemperatureSlider"/>
+
+  	/*onChange={value => this.setState({value})}
+		   onAfterChange={(value, props) => ... } */
+
     return (
       <div className="Thermostat">
-        <input name="slider" type="range" min="1" max="100" value="50" className="TemperatureSlider"/>
+	    <Roundy
+		   value={this.state.readout}
+		   min={0}
+		   max={100}
+		   step={1}
+		   radius={155}
+		   sliced={false}
+		   strokeWidth={25}
+		   onChange={value => this.setState({readout: value})}
+		   color='#8c8c8c'
+		   bgColor='#cdcdcd'
+		   overrideStyle={ 'position:absolute; top: 1.6%;'}
+		/>
         <div className="Temperature">
           <div className="TemperatureReading">
             <input type="text" className="Readout" value={this.state.readout} onChange={this.handleChange}/>
